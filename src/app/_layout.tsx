d54294d12f4f11762d9bcf0d3b6820c3c61908cc
Giuseppe Lanzillotta <glanzillotta@cybsec.it>
2025-06-16 14:41:15 +0200

    Refactor the layout component to improve keyboard handling and integrate status bar management

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,17 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {useFonts} from 'expo-font';
+import {Stack, useSegments} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
-import { KeyboardProvider } from 'react-native-keyboard-controller';
+import {KeyboardProvider} from 'react-native-keyboard-controller';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import {useColorScheme} from '@/components/useColorScheme';
+import {StatusBar} from "react-native";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 
@@ -18,38 +19,43 @@ export {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+    return <RootLayoutNav/>;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+const segment = useSegments()
+    const page = segment[segment.length - 1];
 
-  return (
-    <KeyboardProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="chat" options={{ headerShown: false }} />
-        </Stack>
-    </KeyboardProvider>
-  );
+
+    return (
+        <>
+            <StatusBar barStyle='light-content' backgroundColor={'chat'.includes(page)  ? '#007AFF' : '#f5f5f5'} />
+            <KeyboardProvider>
+                <Stack>
+                    <Stack.Screen name="index" options={{headerShown: false}}/>
+                    <Stack.Screen name="chat" options={{headerShown: false}}/>
+                </Stack>
+            </KeyboardProvider>
+        </>
+    );
 }
